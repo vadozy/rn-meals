13afbe24180ab24a0f0c4990af0146d614af8600
Vadim Storozhuk <vadozy@yahoo.com>
2021-06-20 11:07:33 -0400

    wip after 117

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     Font.loadAsync({
+  //       'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+  //       'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  //     }).then((data) => resolve(data));
+  //   }, 5000);
+  // });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+  return <MealsNavigator />;
+}
